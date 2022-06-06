1d73810e7b0e4ff08874724773ceed3302e6a08b
Felipe Diniz <fdsampaio@yahoo.com.br>
2022-06-06 08:08:39 -0300

    Ajustes nos efeitos

--- oJogo.js
+++ oJogo.js
@@ -9,14 +9,16 @@ const startButton = document.querySelector("#comecar");
 const guessButton = document.querySelector("#chutar");
 
 startButton.addEventListener("click", mudarNome); 
-
 function mudarNome(){
 
     nome = document.getElementById("usuario");
     document.querySelector("#nome-usuario").innerHTML = "Olá, " + nome.value + "!";
+    
     dificuldade = parseInt(document.querySelector("#dificuldade").value);
     document.querySelector("#dica").innerHTML = "Para equilibrar o jogo, lhe direi se a resposta é menor ou maior que o seu chute...";
+    
     document.querySelector("#guess").value = "";
+    document.querySelector(".laugh").play();
 
     victory = 0
     
@@ -24,35 +26,26 @@ function mudarNome(){
         tentativas = 3
         document.querySelector("#tentativas").innerHTML = "Você tem " + tentativas + " tentativas";
     }
-    
     if(dificuldade===0){
         answer=Math.floor((Math.random() * 10) + 1);
     }
-
     if(dificuldade===1){
         answer=Math.floor((Math.random() * 100) + 1);
     }
-
     if(dificuldade===2){
         answer=Math.floor((Math.random() * 200) + 1);
     }
-
     if(dificuldade===3){
         answer=Math.floor((Math.random() * 1000) + 1);
-        tentativas = 8;
+        tentativas = 10;
         document.querySelector("#tentativas").innerHTML = "Você tem " + tentativas + " tentativas";
     }
-
-
 }
 
 guessButton.addEventListener("click", chutar);
-
-
 function chutar(){
 
     chute = parseInt(document.querySelector("#guess").value);
-
 
     if(tentativas > 0 && victory != 1){
         
@@ -61,22 +54,23 @@ function chutar(){
             victory = 1;
             document.querySelector("#tentativas").innerHTML = "Não terá tanta sorte da próxima vez...";
             document.querySelector(".victory").play();
-            setTimeout(function(){document.querySelector(".victory2").play()}, 4000);
+            setTimeout(function(){document.querySelector(".victory2").play()}, 3500);
         }
         else if(chute < answer){
             document.getElementById("dica").innerHTML = "A resposta é maior...";
             tentativas--;             
         }
-        else{
-            document.getElementById("dica").innerHTML = "A resposta é menor...";
-            tentativas--;
-        }
-        
+            else{
+                document.getElementById("dica").innerHTML = "A resposta é menor...";
+                tentativas--;
+            }
         if(tentativas != 0 && victory != 1){
             document.querySelector("#tentativas").innerHTML = "Você tem " + tentativas + " tentativas";
+            document.querySelector(".laugh").play();
+            document.querySelector("#guess").value = "";
         }
-         
     }
+    
     if(tentativas ===0){
         document.querySelector("#dica").innerHTML = "Você perdeu!";
         document.querySelector("#tentativas").innerHTML = "A resposta era " + answer + "!";
